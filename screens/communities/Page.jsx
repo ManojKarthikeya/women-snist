87f84e920009adabf579124e9ddcb24fdcb23f2a
manojkarthikeya <manojkarthikeya@gmail.com>
2023-03-24 21:21:24 +0530

    added com pages

--- screens/communities/Page.jsx
+++ screens/communities/Page.jsx
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Page() {
-  return (
-    <View>
-      <Text>Page</Text>
-    </View>
-  )
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<View style={{flexDirection : 'row', alignItems : 'center', justifyContent : 'space-between'}}>
+				<Text
+					style={{
+						fontFamily: "Gloock-Regular",
+						fontSize: 27,
+						padding: 20,
+						paddingLeft: 20,
+					}}
+				>
+					Coummunity name
+				</Text>
+        <MaterialCommunityIcons name='dots-vertical' size={27} style={{paddingRight: 15}}/>
+			</View>
+		</SafeAreaView>
+	);
 }
